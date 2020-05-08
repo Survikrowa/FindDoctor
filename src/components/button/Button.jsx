d1cb7c38a07973e959fa23659da7aa8cc78a0920
Survikrowa <survikrowa@o2.pl>
2020-05-08 10:00:13 +0200

    Add: Reusable button component

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export class Button extends React.Component {
+  static defaultProps = {
+    btnType: "button",
+    btnStyle: "text-white py-2 px-4 rounded"
+  };
+
+  render() {
+    return (
+      <button
+        className={this.props.btnStyle}
+        onClick={this.props.onClick}
+        type={this.props.btnType}
+      >
+        {this.props.content}
+      </button>
+    );
+  }
+}
